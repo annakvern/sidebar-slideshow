15192a194f6cee86bfc8aee908dc1a2e8fcd078f
Anna <anna.kvernplassen@gmail.com>
2024-11-05 15:48:11 +0100

    implement slideshow

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 window.addEventListener("DOMContentLoaded", main);
 
-function main() {}
+let visibleIndex = 0;
+
+function main() {
+  setInterval(changeImage, 3000);
+}
+
+function changeImage() {
+  const images = document.querySelector(".slideshow-container").children; //hämta det första html elementet som matchar det här
+  images[visibleIndex].classList.remove("visible");
+  visibleIndex = (visibleIndex + 1) % images.length;
+  // visibleIndex++;
+  // if (visibleIndex >= images.length) {
+  //   visibleIndex = 0;
+  // }
+  images[visibleIndex].classList.add("visible");
+}
