cadb90750f9a937d779aa60f4be33a3863c16bf1
Anna <anna.kvernplassen@gmail.com>
2024-11-06 11:38:11 +0100

    Implemented sidebar and selected icon toggler

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ let visibleIndex = 0;
 
 function main() {
   setInterval(changeImage, 3000);
+  setupEventListeners();
+}
+
+function setupEventListeners() {
+  const sidebarItems = document.querySelectorAll(".sidebarItem");
+  for (item of sidebarItems) {
+    item.addEventListener("click", showSelected);
+  }
 }
 
 function changeImage() {
@@ -16,3 +24,22 @@ function changeImage() {
   // }
   images[visibleIndex].classList.add("visible");
 }
+
+function showSelected(clickedItem) {
+  // Remove "selected" class from any previously selected item
+  const sidebarItems = document.querySelectorAll(".sidebarItem");
+  for (item of sidebarItems) {
+    item.classList.remove("selected");
+  }
+
+  // Add "selected" class to the clicked item
+  clickedItem.currentTarget.classList.add("selected");
+}
+
+// När bildslidern är på plats är det dags att skapa sidebaren som ska
+// ligga fixerad till vänster. Sidebaren ska kunna fällas ihop på desktop
+// så att bara ikonerna syns. På mobilen skall den gömmas helt och visas när
+// användaren klickar på en ikon som ligger uppe i vänster hörn.
+// När användaren har valt en ikon ska sidebaren fällas ihop på datorn och
+// gömmas helt på mobilen. Tänkt på att dela upp den här punkten i mindre
+// delmål för att lyckas genomföra den.
